4b85db55209a8f3f07ac54d3baeaf6ee875cb032
Surferlul <lukasabaumann@gmail.com>
2021-10-07 14:08:01 +0200

    try adding new awesome version

--- pkgs/applications/window-managers/awesome/default.nix
+++ pkgs/applications/window-managers/awesome/default.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "awesomewm";
     repo = "awesome";
-    rev = "v${version}";
+    rev = "f817237";
     sha256 = "1i7ajmgbsax4lzpgnmkyv35x8vxqi0j84a14k6zys4blx94m9yjf";
   };
 
